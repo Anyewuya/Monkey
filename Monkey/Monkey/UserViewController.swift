41f797e4655d8ac8f2ad493c354829e8a6299723
Sev_mbp <severusllb@qq.com>
2015-06-30 08:31:12 +0800

    init project

---
+++ Monkey/Monkey/UserViewController.swift
@@ -0,0 +1,13 @@
+//
+//  UserViewController.swift
+//  Monkey
+//
+//  Created by 周宇 on 15/6/30.
+//  Copyright (c) 2015年 Dning. All rights reserved.
+//
+
+import UIKit
+
+class UserViewController: UIViewController {
+
+}
---
+++ Monkey/Monkey/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Monkey
+//
+//  Created by 周宇 on 15/6/29.
+//  Copyright (c) 2015年 Dning. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
